a69ba5da653330e04ee0c5ce92d68934e51cbd85
iamjunsulee <mellow0924@gmail.com>
2022-01-29 23:02:27 +0900

    feat : jpa auditing 기능 추가

--- src/main/java/me/junsu/demojpastudy/domain/BaseEntity.java
+++ src/main/java/me/junsu/demojpastudy/domain/BaseEntity.java
@@ -1,13 +1,20 @@
 package me.junsu.demojpastudy.domain;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 //테이블과 매핑할 필요없이 공통으로 사용되는 매핑정보만 제공하기 위함
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseEntity {
+    @CreatedDate
     public LocalDateTime createDate;
 
     @LastModifiedDate
--- src/main/java/me/junsu/demojpastudy/domain/Member.java
+++ src/main/java/me/junsu/demojpastudy/domain/Member.java
@@ -25,8 +25,6 @@ public class Member extends BaseEntity{
     public Member(String name, Address address) {
         this.name = name;
         this.address = address;
-        this.createDate = LocalDateTime.now();
-        this.updateDate = LocalDateTime.now();
     }
 
     //연관관계의 주인이 아님을 명시
